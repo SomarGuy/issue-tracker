50c655c26e0c1cde09f3674de45919688c69010c
SomarGuy <diogo.ramos@me.com>
2023-10-27 15:49:53 -0400

    add the prisma adapter

---
+++ prisma/migrations/20231027194906_add_user_models/migration.sql
@@ -0,0 +1,8 @@
+-- DropForeignKey
+ALTER TABLE `account` DROP FOREIGN KEY `Account_userId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `session` DROP FOREIGN KEY `Session_userId_fkey`;
+
+-- AlterTable
+ALTER TABLE `issue` ADD COLUMN `assignedToUserId` VARCHAR(255) NULL;
