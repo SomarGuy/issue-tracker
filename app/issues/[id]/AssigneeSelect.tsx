145018a0d1540230195547589ca62e412a016d0b
SomarGuy <diogo.ramos@me.com>
2023-10-28 11:30:48 -0400

    Refactor assigneeselectcomponent

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,31 +1,28 @@
 'use client';
 
-import { Skeleton } from '@/app/components'
+import { Skeleton } from '@/app/components';
 import { Issue, User } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import toast, { Toaster } from 'react-hot-toast'
+import toast, { Toaster } from 'react-hot-toast';
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-    const { data: users, error, isLoading } = useQuery<User[]>({
-        queryKey: ['users'],
-        queryFn: () => axios.get('/api/users').then(res => res.data),
-        staleTime: 60 * 1000, //60s
-        retry: 3
-    });
+    const { data: users, error, isLoading } = useUsers();
 
     if (isLoading) return <Skeleton />
 
     if (error) return null;
 
-  return (
-    <>
-    <Select.Root defaultValue={issue.assignedToUserId || ''} onValueChange={(userId) => {
+    const assignIssue = (userId: string) => {
         axios.patch('/api/issues/' + issue.id, { assignedToUserId: userId || null}).catch(() => {
             toast.error('Changes could not be saved.');
         })
-    }}>
+    }
+
+  return (
+    <>
+    <Select.Root defaultValue={issue.assignedToUserId || ''} onValueChange={assignIssue}>
         <Select.Trigger placeholder='Assign...' />
             <Select.Content>
                 <Select.Group>
@@ -39,7 +36,14 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </Select.Root>
     <Toaster />
     </>
-  )
-}
+  );
+};
+
+const useUsers = () => useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get('/api/users').then(res => res.data),
+    staleTime: 60 * 1000, //60s
+    retry: 3
+});
 
 export default AssigneeSelect
