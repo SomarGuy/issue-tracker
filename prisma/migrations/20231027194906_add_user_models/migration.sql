8690d40f1a8b7b2ebc691a40056d05b2bc76bf66
SomarGuy <diogo.ramos@me.com>
2023-11-06 23:37:45 -0500

    Change relation mode to prisma

--- prisma/migrations/20231027194906_add_user_models/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `account` DROP FOREIGN KEY `Account_userId_fkey`;
-
--- DropForeignKey
-ALTER TABLE `session` DROP FOREIGN KEY `Session_userId_fkey`;
-
--- AlterTable
-ALTER TABLE `issue` ADD COLUMN `assignedToUserId` VARCHAR(255) NULL;
--- prisma/migrations/20231028150618_add_assigned_issues/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX `Account_userId_fkey` ON `account`;
-
--- DropIndex
-DROP INDEX `Session_userId_fkey` ON `session`;
